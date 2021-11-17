c583b6c7f82892a1f79154592351a60b0e12aa76
vnyvny@gmail.com <vnyvny@gmail.com>
2021-11-17 19:43:49 +0000

    add space

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,6 +1,6 @@
 
 /*
-    Welcome to your first dbt model!
+    Wel-come to your first dbt model!
     Did you know that you can also configure models directly within SQL files?
     This will override configurations stated in dbt_project.yml
 
