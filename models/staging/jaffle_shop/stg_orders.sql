a90b82ad17d8a94adddaf210457ad8a995310644
vnyvny@gmail.com <vnyvny@gmail.com>
2021-11-19 21:17:07 +0000

    sources and test docs

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -5,7 +5,8 @@ with orders as (
         user_id as customer_id,
         order_date,
         status
-    from raw.jaffle_shop.orders
+    --from raw.jaffle_shop.orders
+    from {{ source('jaffle_shop', 'orders') }}
 
 )
 
